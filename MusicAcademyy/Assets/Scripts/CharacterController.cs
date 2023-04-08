ba0f0731db1264f218c02764a3f021622c301e90
Girayhan Çakıroğlu <girayhancakiroglu7@gmail.com>
2023-04-08 17:44:54 +0300

    nota animasyonu eklendi.

--- MusicAcademyy/Assets/Scripts/CharacterController.cs
+++ MusicAcademyy/Assets/Scripts/CharacterController.cs
@@ -6,17 +6,17 @@ using UnityEngine.EventSystems;
 
 public class CharacterController : MonoBehaviour
 {
-    public float moveSpeed = 1f;
-    public float runSpeed = 2f;
-    public float rotationSpeed = 10.0f;
+    public float moveSpeed = 1.0f;
+    public float runSpeed = 2.0f;
     private bool run = false;
+    
     private Animator _animator;
+    
     void Start()
     {
         _animator = GetComponent<Animator>();
     }
 
-    
     void Update()
     {
         float yatay = Input.GetAxis("Horizontal");
@@ -27,15 +27,24 @@ public class CharacterController : MonoBehaviour
             run = true;
         }
         else
+        {
             run = false;
-
-        Vector3 hareket = new Vector3(yatay, 0.0f, dikey);
+        }
+        
+        Vector3 hareket = new Vector3(yatay,0.0f, dikey);
         hareket = transform.TransformDirection(hareket);
 
         float speed = run ? runSpeed : moveSpeed;
         _animator.SetFloat("speed",speed* (Math.Abs(dikey)+Math.Abs(yatay)));
-        
-        Debug.Log(speed);
-        transform.position += hareket * speed * Time.deltaTime;
+        if (hareket != Vector3.zero)
+        {
+            transform.Translate(hareket * speed * Time.deltaTime,Space.World);
+            Quaternion targetRotation = Quaternion.LookRotation(hareket);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 0.01f);
+        }
+        else
+        {
+            _animator.SetFloat("speed", 0.0f);
+        }
     } 
 }
